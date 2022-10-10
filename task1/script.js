00422ffdbe3ed70854a2a9825e99f5dac96eae74
Suren Balayan <surenbalayan@Surens-MacBook-Pro.local>
2022-10-10 19:27:32 +0400

    v1

---
+++ task1/script.js
@@ -0,0 +1,40 @@
+const array = [
+    {
+        name: 'Tom',
+        age: 23
+    },
+    {
+        name: 'John',
+        age: 19
+    },
+    {
+        name: 'Ann',
+        age: 23
+    },
+    {
+        name: 'Gaya',
+        age: 32
+    },
+    {
+        name: 'David',
+        age: 19
+    },
+];
+
+let groupByAge = arr => {
+    let obj = {}
+    arr.forEach(el => {
+        let a = el.age;
+        if(!obj[a]){
+           obj[a] = [];
+           obj[a].push(el) 
+        }else{
+            obj[a].push(el) 
+        }
+        
+    });
+    return obj;
+}
+
+
+console.log(groupByAge(array));
